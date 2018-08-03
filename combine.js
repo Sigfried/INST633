edff92bf1be13b0decabf321cc2d13c1b29702f2
Sigfried Gold <sigfried@sigfried.org>
2018-08-03 12:15:59 -0400

    maded coded only gephi. not great so far

--- combine.js
+++ combine.js
@@ -1,6 +1,7 @@
 'use strict'
 var fs = require('fs')
 const neatCsv = require('neat-csv')
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const _ = require('supergroup')
 const should = require('should')
 
@@ -9,6 +10,7 @@ const codingaf = fs.createReadStream('./Copy of Roseanne Coding - A-J.tsv')
 const codingbf = fs.createReadStream('./Copy of Roseanne Coding - K-Z.tsv')
 
 //let edges =[], codinga = [], codingb = []
+let Nodes = {}
 
 Promise.all([
   neatCsv(edgef),
@@ -17,6 +19,18 @@ Promise.all([
 ]).then(
   ([edges, codinga, codingb]) => {
     processData({edges, codinga, codingb})
+
+    // output nodes
+    let gephiNodes = _.values(Nodes).map(node => node.gephiNode())
+    //console.log(gephiNodes.filter(n=>n.tweets > 1))
+    //gephiNodes.filter(n=>n.tweets > 1).map(d=>JSON.stringify(d,null,0)).forEach(d=>console.log(d,'\n'))
+    writeNodes(gephiNodes)
+
+    _.filter(Nodes, (n,id)=>n.tweets.length > 1)
+      .map((n,id) => console.log(
+                        `${n.id}, ${n.tweets.length} tweets, targets: `,
+                        JSON.stringify(n.tweets.map(t=>t.targets)), '\n'))
+    writeEdges(_.flatten(_.values(Nodes).map(n=>n.gephiEdges())))
   }
 ).catch((err,a,b) => console.error('ERROR!', {err, a, b}))
 
@@ -24,7 +38,6 @@ let usersMissingFromEdges = {}
 let targetsMissingFromEdges = {}
 let notMissing = 0
 
-let Nodes = {}
 class Node {
   constructor(id) {
     if (Nodes[id]) {
@@ -56,31 +69,85 @@ class Node {
     return _.values(Nodes)
   }
   gephiNode() {
-    return {
+    let rec = {
       ID: this.id,
       tweets: this.tweets.length,
       mentions: this.mentions.length,
-      ..._.mapKeys(
-        _.mapValues(_.groupBy(this.tweets.map(t=>t.code1)), v => v.length),
-        (v,k) => `code1_${k}`),
-      ..._.mapKeys(
-        _.mapValues(_.groupBy(this.tweets.map(t=>t.code2)), v => v.length),
-        (v,k) => `code2_${k}`),
     }
+    CODES.forEach(code => {
+      this['raw-' + code] = [ this.tweets.map(t=>(t.code1 === code)+0),
+                             this.tweets.map(t=>(t.code2 === code)+0) ]
+      this[code] = 
+        _.range(rec.tweets)
+          .map(i => (this[`raw-${code}`][0][i] & this[`raw-${code}`][1][i]))
+    })
+    let denominator = rec.tweets + 0.0
+    rec.pro = (_.sum(this['Pro-Roseanne']) / denominator) || ''
+    rec.anti = (_.sum(this['Anti-Roseanne']) / denominator) || ''
+    rec.neutral = (_.sum(this['Neutral']) / denominator) || ''
+    return rec
   }
+  gephiEdges() {
+    return _.flatten(this.tweets.map(
+      t => t.targets.map(tg => ({Source: this.id, Target:tg, Date: t.date}))
+    ))
+  }
+}
+const CODES = [
+      'Pro-Roseanne',
+      'Anti-Roseanne',
+      'Neutral',
+      'Unclear/Unrelated',
+]
+function writeNodes(nodes) {
+  //console.log('weird?', JSON.stringify(nodes[0]))
+  const csvWriter = createCsvWriter({
+    path: './coded-only/coded-only-nodes.csv',
+    header: [
+      {id: 'ID', title: 'ID'},
+      {id: 'tweets', title: 'tweets'},
+      {id: 'mentions', title: 'mentions'},
+      {id: 'pro', title: 'pro'},
+      {id: 'anti', title: 'anti'},
+      {id: 'neutral', title: 'neutral'},
+    ],
+  });
+
+  csvWriter.writeRecords(nodes)       // returns a promise
+      .then(() => {
+          console.log('...Done writing');
+      });
+}
+function writeEdges(edges) {
+  //console.log('weird?', JSON.stringify(edges[0]))
+  const csvWriter = createCsvWriter({
+    path: './coded-only/coded-only-edges.csv',
+    header: [
+      {id: 'Source', title: 'Source'},
+      {id: 'Target', title: 'Target'},
+      {id: 'Date', title: 'Date'},
+    ],
+  });
+
+  csvWriter.writeRecords(edges)       // returns a promise
+      .then(() => {
+          console.log('...Done writing');
+      });
 }
 
 function processData({edges, codinga, codingb}) {
 
-  _.each(edges, edge => {
-    let src = new Node(edge.Source)
-    src.addTarget(edge.Target)
-    let tgt = new Node(edge.Target)
-    tgt.addSource(edge.Source)
-  })
-  //console.log({ Nodes, Nodeslen: _.values(Nodes).length})
-  //logstr(Node.vals().slice(200, 250), 2000)
-  //logstr(Nodes, 5000)
+  if (false) {    // skip netw stuff for now
+    _.each(edges, edge => {
+      let src = new Node(edge.Source)
+      src.addTarget(edge.Target)
+      let tgt = new Node(edge.Target)
+      tgt.addSource(edge.Source)
+    })
+    //console.log({ Nodes, Nodeslen: _.values(Nodes).length})
+    //logstr(Node.vals().slice(200, 250), 2000)
+    //logstr(Nodes, 5000)
+  }
 
   _.each(codinga, (rec, i) => {
     let {'Assigned To':coder1, Code: code1,
@@ -104,19 +171,28 @@ function processData({edges, codinga, codingb}) {
     })
   })
 
-  // output nodes
-
-  let gephiNodes = _.values(Nodes).map(node => node.gephiNode())
-  console.log(gephiNodes)
+}
 
 
-  /*
-  let sampleDumps = Node.vals().slice(300, 350).map(
-    n => n.tweets.map( t => console.log(t.tweet, '\n'))
+function checkData(things) {
+  _.each(things,
+    (v,k) => {
+      try {
+        console.log(`${k} ${typeof(v)} ${v.length}`)
+        logstr(v)
+      } catch(e) {
+        console.error('error trying to process!')
+      }
+    }
   )
-  */
-  process.exit()
+  let {edges, codinga, codingb} = things
+  should(codinga.length).equal(codingb.length)
+}
 
+function logstr(o, len=100, indent=2) {
+  let str = JSON.stringify(o, null, indent)
+  console.log(str.slice(0, Math.min(len, str.length)))
+}
   //checkData({edges, codinga, codingb})
   /*
   let tweets = addTweets(ppl, codinga, codingb)
@@ -168,25 +244,3 @@ function processData({edges, codinga, codingb}) {
   console.log(ppl)
   logstr(tweets, 500)
   */
-}
-
-
-function checkData(things) {
-  _.each(things,
-    (v,k) => {
-      try {
-        console.log(`${k} ${typeof(v)} ${v.length}`)
-        logstr(v)
-      } catch(e) {
-        console.error('error trying to process!')
-      }
-    }
-  )
-  let {edges, codinga, codingb} = things
-  should(codinga.length).equal(codingb.length)
-}
-
-function logstr(o, len=100, indent=2) {
-  let str = JSON.stringify(o, null, indent)
-  console.log(str.slice(0, Math.min(len, str.length)))
-}
